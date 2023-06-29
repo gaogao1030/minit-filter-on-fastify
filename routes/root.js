a9791d6f2dfdb48a05865501621e4dd595fa9e84
ガオガオ <gaogao1130@gmail.com>
2023-06-30 01:27:21 +0800

    过滤特殊符号提取有效内容做敏感词验证

--- routes/root.js
+++ routes/root.js
@@ -7,13 +7,17 @@ module.exports = async function (fastify, opts) {
 
   fastify.post('/replace', async (request, reply) => {
     const mint = fastify.get_mint()
-    const status = mint.filter(request.body.content)
-    return status
+    const content = request.body.content
+    const result = content.match(/[\u4e00-\u9fa5A-Za-z0-9]+/g).join('')
+    const response = mint.filter(result)
+    return response
   })
 
   fastify.post('/verify', async (request, reply) => {
     const mint = fastify.get_mint()
-    const status = mint.verify(request.body.content)
+    const content = request.body.content
+    const result = content.match(/[\u4e00-\u9fa5A-Za-z0-9]+/g).join('')
+    const status = mint.verify(result)
     return { verify: status}
   })
 }
